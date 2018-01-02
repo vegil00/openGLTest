2509e861b0d5d1eaef4609e022e899638689b0bc
vegil00 <30295230+vegil00@users.noreply.github.com>
2018-01-02 22:38:46 +0800

    change1

--- openGLTest/openGLTest/openGLTest.cpp
+++ openGLTest/openGLTest/openGLTest.cpp
@@ -9,6 +9,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<vector>
+#include<iostream>
+using namespace std;
 //void lineSegment()
 //{
 //	glClear(GL_COLOR_BUFFER_BIT);
@@ -28,10 +31,11 @@ void lineSegment()
 {
 	glClear(GL_COLOR_BUFFER_BIT);           // Clear display window
 
-	glColor3f(1.0, 0.0, 0.0);               // Set line segment color to red
-	glBegin(GL_LINES);
-	glVertex2i(180, 15);
-	glVertex2i(10, 145);
+	glColor3f(1.0, 0.0, 0.0);  
+	//glPointSize(100);// Set line segment color to red
+	glBegin(GL_POINTS);
+
+	glVertex3f(100, 75, 0);
 	glEnd();
 
 	glFlush();                              // Process all OpenGL routines as quickly as possible
@@ -49,21 +53,76 @@ void lineSegment()
 //	glutDisplayFunc(lineSegment);           // Send graphics to display window
 //	glutMainLoop();                         // Display everything and wait
 //}
+void strcopy(char* des, char* source, int size)
+{
+	//if (des != nullptr)
+	//{
+	//	delete des;
+	//	des = nullptr;
+	//}
+	//*(&des )= new char[size + 1];
+	if (des == nullptr || source == nullptr)
+	{
+		return;
+	}
+	//for (int i = 0; i < size+1; i++)
+	//{
+	//	des[i] = source[i];
+	//}
+	if (des<source || des>source + size + 1)
+	{
+		while ((*source) != '\0');
+			
+		{
+			*des++ = *(source++);
+			} 
+		*(des ) = '\0';
+	}
+	else
+	{
+		source = source + size-1 ;
+		des = des +size-1 ;
+		int temp = size;
+		while (temp--)
+		{
+			*des-- = *source--;
+			
+		}
+		des[size + 1] = '\0';
+	}
+	
+	//memcpy(des, source, size+1);
+}
+int strlength(char* str)
 
+{
+	int length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return length;
+}
 int main(int argc,char* argv[])
 
 {
-	
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(50, 100);
-	glutInitWindowSize(400, 300);
-	glutCreateWindow("opengl");
-	glClearColor(1.0, 1.0, 1.0, 0);
-	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0, 200, 0, 150);
-	glutDisplayFunc(lineSegment);
-	glutMainLoop();
+	vector<int>v;
+	//v.insert(v.begin(), 1);
+	//glutInit(&argc, argv);
+	//glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	//glutInitWindowPosition(50, 100);
+	//glutInitWindowSize(400, 300);
+	//glutCreateWindow("opengl");
+	//glClearColor(1.0, 1.0, 1.0, 0);
+	//glMatrixMode(GL_PROJECTION);
+	//gluOrtho2D(0, 200, 0, 150);
+	//glutDisplayFunc(lineSegment);
+	//glutMainLoop();
+	char* a = "123456";
+	char*b=new char;
+	strcopy(b, a, strlen(a));
+	cout << b << endl;
+	cout << strlength(b)<<endl;
 	system("pause");
 	return 1;
     
